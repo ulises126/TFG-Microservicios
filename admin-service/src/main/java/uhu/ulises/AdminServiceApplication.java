5667bfac66b30380a82b1703c6905f3683f3d975
ulises126 <73275755+ulises126@users.noreply.github.com>
2022-06-15 20:45:44 +0200

    admin funcionando

--- admin-service/src/main/java/uhu/ulises/AdminServiceApplication.java
+++ admin-service/src/main/java/uhu/ulises/AdminServiceApplication.java
@@ -3,6 +3,9 @@ package uhu.ulises;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@EnableAdminServer
 @SpringBootApplication
 public class AdminServiceApplication {
 
